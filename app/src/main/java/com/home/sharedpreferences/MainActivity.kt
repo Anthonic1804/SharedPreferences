79a4ddab3e307f700bc7215ff47862099ab43f8e
Antonio Hernández <54154901+Anthonic1804@users.noreply.github.com>
2023-02-06 07:21:54 -0600

    CREANDO METODO INITUI

--- app/src/main/java/com/home/sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/home/sharedpreferences/MainActivity.kt
@@ -2,10 +2,33 @@ package com.home.sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private var btnContinuar: Button? = null
+    private var etNombre: EditText? = null
+    private var cbVip: CheckBox? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnContinuar = findViewById(R.id.btncontinuar)
+        etNombre = findViewById(R.id.etNombre)
+        cbVip = findViewById(R.id.cbVip)
+
+        initUI()
+    }
+
+
+    fun initUI(){
+        btnContinuar!!.setOnClickListener{ accessToDetail() }
+    }
+
+    fun accessToDetail(){
+
     }
 }
